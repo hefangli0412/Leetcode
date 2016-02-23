294498552f3e6c78abff68a83bf6b38667336c13
Hefang Li <hefangli0412@users.noreply.github.com>
2016-02-22 21:43:04 -0800

    Create 116.java

---
+++ 116.java
@@ -0,0 +1,45 @@
+// 116. Populating Next Right Pointers in Each Node
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        // level order traversal
+        if (root == null) return;
+        
+        Queue<TreeLinkNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeLinkNode prev = null;
+            for (int i = 0; i < size; i++) {
+                TreeLinkNode cur = queue.poll();
+                
+                // process cur
+                if (prev == null) {
+                    prev = cur;
+                } else {
+                    prev.next = cur;
+                    prev = cur;
+                }
+                
+                // process cur's children
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            prev.next = null;
+        }
+    }
+}
