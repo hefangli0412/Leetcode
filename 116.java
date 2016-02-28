61bfcfc694d28bff86cfdff743aa3ca42a555b0f
Hefang Li <hefangli0412@users.noreply.github.com>
2016-02-28 13:01:12 -0800

    Update 116.java

--- 116.java
+++ 116.java
@@ -10,6 +10,52 @@
  * }
  */
 public class Solution {
+    /* level order traversal - 
+       since only constant extra space could be used,
+       queue cannot be used here
+    */
+    public void connect(TreeLinkNode root) {
+
+        /* node represents the node being processed;
+           root indicates the root of the current node, 
+           it moves to its right on the same level;
+           leftMost represents the left most node in the current level,
+           we assign it to root at the start of the next level
+           */
+        for (TreeLinkNode leftMost = root; leftMost != null; ) {
+            TreeLinkNode newLeftMost = null; 
+            TreeLinkNode tail = null;
+            
+            for (TreeLinkNode parent = leftMost; parent != null; parent = parent.next) {
+                if (parent.left != null) {
+                    if (newLeftMost == null) {
+                        newLeftMost = parent.left;
+                        tail = newLeftMost;
+                    } else {
+                        tail.next = parent.left;
+                        tail = tail.next;
+                    }
+                }
+                
+                if (parent.right != null) {
+                    if (newLeftMost == null) {
+                        newLeftMost = parent.right;
+                        tail = newLeftMost;
+                    } else {
+                        tail.next = parent.right;
+                        tail = tail.next;
+                    }
+                }
+            }
+            
+            leftMost = newLeftMost;
+        }
+    }
+}
+
+/* previous solution */
+
+public class Solution {
     public void connect(TreeLinkNode root) {
         // level order traversal
         if (root == null) return;
