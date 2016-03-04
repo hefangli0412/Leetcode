3b614c508c9fe1fb348db9a05f0d834ac70f4d69
Hefang Li <hefangli0412@users.noreply.github.com>
2016-03-04 09:41:51 -0800

    Update 227.java

--- 227.java
+++ 227.java
@@ -1,42 +1,43 @@
 // 227. Basic Calculator II
 // https://leetcode.com/problems/basic-calculator-ii/
 
+/* 注意判断什么时候可以evaluate *、
+
 /* Solution 1 : use preVal to record previous value evaluated so far */
 
 public class Solution {
     public int calculate(String s) {
         int result = 0;
-        int preVal = 0;
         char sign = '+';
+        int preVal = 0;
         
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (ch == ' ') {
                 continue;
             } else if (Character.isDigit(ch)) {
-                int curVal = ch - '0';
+                int num = ch - '0';
                 while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
                     i++;
-                    curVal = curVal * 10 + s.charAt(i) - '0';
+                    num = num * 10 + s.charAt(i) - '0';
                 }
                 if (sign == '+') {
                     result += preVal;
-                    preVal = curVal;
+                    preVal = num;
                 } else if (sign == '-') {
                     result += preVal;
-                    preVal = -curVal;
+                    preVal = -num;
                 } else if (sign == '*') {
-                    preVal = preVal * curVal;
+                    preVal *= num;
                 } else if (sign == '/') {
-                    preVal = preVal / curVal;
-                } 
+                    preVal /= num;
+                }
             } else {
                 sign = ch;
             }
         }
         
         result += preVal;
-        
         return result;
     }
 }
