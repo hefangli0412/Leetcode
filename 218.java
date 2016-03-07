583277a529111b5c332c4f2881b653077eae63d7
Hefang Li <hefangli0412@users.noreply.github.com>
2016-03-06 16:40:50 -0800

    Update 218.java

--- 218.java
+++ 218.java
@@ -1,7 +1,17 @@
 // 218. The Skyline Problem
 // https://leetcode.com/problems/the-skyline-problem/
 
-/* for position in sorted(all start points and all end points)
+/* Refer to https://leetcode.com/discuss/67091/once-for-all-explanation-with-clean-java-code-nlog-time-space
+
+https://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
+PriorityQueue has a remove(Object) method, which takes O(n) time.
+
+The time complexity is O(n log n + n * k).
+k is the max size of priority queue.
+
+The general idea is - 
+
+for position in sorted(all start points and all end points)
  if this position is a start point
         add its height
  else if this position is a end point
@@ -41,7 +51,7 @@ public class Solution {
     	});
     
     	// Use a maxHeap to store possible heightss
-    	Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    	PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
     
       // Provide a initial value to make pq not empty during processing
     	pq.offer(0);
@@ -68,3 +78,14 @@ public class Solution {
     	return result;
     }
 }
+
+/* Other good solutions - https://leetcode.com/discuss/48638/java-570ms-430ms-divide-conquer-solution-with-explanation
+
+This solution uses TreeMap instead of PriorityQueue, do give it a look!
+
+The key point is to use height as the key.
+
+https://docs.oracle.com/javase/7/docs/api/java/util/TreeMap.html
+A Red-Black tree based NavigableMap implementation.
+This implementation provides guaranteed log(n) time cost for the containsKey, get, put and remove operations. 
+*/
