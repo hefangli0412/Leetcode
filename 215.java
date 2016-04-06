957627d198845d174e263442e0461f755c4d620e
Hefang Li <hefangli0412@users.noreply.github.com>
2016-04-06 08:10:14 -0700

    Update 215.java

--- 215.java
+++ 215.java
@@ -16,66 +16,49 @@ Be consistent and let kth represents kth element in the original array.
 
 public class Solution {
     
-    public int findKthLargest(int[] nums, int k)
-    {
+    public int findKthLargest(int[] nums, int k) {
         return findKthLargest(nums, 0, nums.length - 1, k);
     }
 
     // k is 1-based
-    // kth element in the original array
-    private int findKthLargest(int[] array, int left, int right, int k)
-    {
-        if (left >= right) 
-        {
-            return array[left];
+    // k - 1 index element in the array
+    private int findKthLargest(int[] array, int left, int right, int k) {
+        if (left >= right) {
+            return array[k - 1];
         }
         
         int pivot = partition(array, left, right);
-        if (pivot == k - 1)
-        {
+        if (pivot == k - 1) {
             return array[pivot];
-        }
-        else if (pivot < k - 1)
-        {
+        } else if (pivot < k - 1) {
             return findKthLargest(array, pivot + 1, right, k);
-        }
-        else
-        {
+        } else {
             return findKthLargest(array, left, pivot - 1, k);
         }
     }
     
     // right must be greater than left
-    private int partition(int[] array, int left, int right)
-    {
+    private int partition(int[] array, int left, int right) {
         int pivotIndex = left + (int) ((right - left + 1) * Math.random());
         int pivot = array[pivotIndex];
-        // swap the pivot to the rightmost position
         swap(array, pivotIndex, right);
 
-        int i = left; // the left side of i are all smaller than pivot(exclusive)
-        int j = right - 1; // the right side of j are all greater than or equal to pivot(exclusive)
-        while (i <= j)
-        {
-            if (array[i] > pivot)
-            {
+        int i = left;
+        int j = right - 1;
+        while (i <= j) {
+            if (array[i] > pivot) {
                 i++;
-            }
-            else if (array[j] <= pivot)
-            {
+            } else if (array[j] <= pivot) {
                 j--;
-            }
-            else
-            {
+            } else {
                 swap(array, i++, j--);
             }
         }
-        swap(array, i, right); // i is in the rightside part (>=)
+        swap(array, i, right);
         return i;
     }
     
-    private void swap(int[] array, int x, int y)
-    {
+    private void swap(int[] array, int x, int y) {
         int temp = array[x];
         array[x] = array[y];
         array[y] = temp;
