345fd9639071f7b79cbecdbe8cc3ddbf201bbeec
Hefang Li <hefangli0412@users.noreply.github.com>
2016-04-27 12:20:06 -0700

    Update 188.java

--- 188.java
+++ 188.java
@@ -39,35 +39,52 @@ public int maxProfit(int k, int[] prices) {
 }
 
 // optimize space
-public int maxProfit(int k, int[] prices) {
-    int n = prices.length;
+public class BuyAndSellStockKSolver {
+	public int maxProfit(int[] prices, int k) {
+		if (prices.length < 2 || k == 0) {
+			return 0;
+		}
+		
+		if (k >= prices.length / 2) {
+			int res = 0;
+			for (int i = 1; i < prices.length; i++) {
+				if (prices[i] > prices[i - 1]) {
+					res += prices[i] - prices[i - 1];
+				}
+			}
+			return res;
+		}
+		
+		int[] pre = new int[prices.length];
+		int[] cur = new int[prices.length];
+		for (int i = 1; i <= k; i++) {
+			// swap pre and cur
+			int[] tmp = pre;
+			pre = cur;
+			cur = tmp;
+			
+			int localMax = -prices[0];
+			for (int j = 1; j < prices.length; j++) {
+				cur[j] = Math.max(cur[j - 1], prices[j] + localMax);
+				// 下一个循环做减法时，j是last buy in
+				localMax = Math.max(localMax, pre[j - 1] - prices[j]);
+			}
+		}
+		
+		return cur[prices.length - 1];
+	}
+}
 
-    if (n <= 1 || k == 0) {
-        return 0;
+public class BuyAndSellStockKSolverTest {
+	BuyAndSellStockKSolver solver = new BuyAndSellStockKSolver();
+	
+    @Test
+	public void test1() {
+    	assertEquals(38, solver.maxProfit(new int[] {5, -10, 1, 2, 6, -1, -2, 20, 2}, 2));
     }
 
-    // if k >= n/2, then you can make maximum number of transactions.
-    if (k >=  n/2) {
-        int maxPro = 0;
-        for (int i = 1; i < n; i++) {
-            if (prices[i] > prices[i-1])
-                maxPro += prices[i] - prices[i-1];
-        }
-        return maxPro;
-    }
-
-    int[] pre = new int[n];
-    int[] cur = new int[n];
-    for (int i = 1; i <= k; i++) {
-        // swap pre and cur
-        int[] tmp = pre;
-        pre = cur;
-        cur = tmp;
-        int localMax = - prices[0];
-        for (int j = 1; j < n; j++) {
-            cur[j] = Math.max(cur[j - 1],  prices[j] + localMax);
-            localMax = Math.max(localMax, pre[j - 1] - prices[j]);
-        }
-    }
-    return cur[n - 1];
+    @Test
+	public void test2() {
+		assertEquals(7, solver.maxProfit(new int[] {3,2,6,5,0,3}, 2));
+	}
 }
