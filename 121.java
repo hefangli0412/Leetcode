7a23f98ba5ba8844c85684300645f6f8a1615b54
Hefang Li <hefangli0412@users.noreply.github.com>
2016-05-11 11:33:45 -0700

    Update 121.java

--- 121.java
+++ 121.java
@@ -1,18 +1,21 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-public class BuyAndSellStockOneSolver {
+public class Solution {
     public int maxProfit(int[] prices) {
-    	if (prices.length <= 1) {
-    		return 0;
-    	}
-    	
-    	int res = 0;
-    	int min = prices[0];
-    	for (int i = 1; i < prices.length; i++) {
-    		res = Math.max(res, prices[i] - min);
-    		min = Math.min(min, prices[i]);
-    	}
-    	
-    	return res;
+        if (prices.length < 2) {
+            return 0;
+        }
+        
+        // M[i] denotes the largest sum of a subarray of 1,2,..,i ending at i.
+        int[] M = new int[2];
+        int global_max = 0;
+        
+        M[0] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            M[i % 2] = Math.max(0, M[(i - 1) % 2] + prices[i] - prices[i - 1]);
+            global_max = Math.max(global_max, M[i % 2]);
+        }
+        
+        return global_max;
     }
 }
