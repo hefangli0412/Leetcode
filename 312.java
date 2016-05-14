5de920c9d3070a888853f65d4631b09a08725e62
Hefang Li <hefangli0412@users.noreply.github.com>
2016-05-14 09:08:03 -0700

    Update 312.java

--- 312.java
+++ 312.java
@@ -1,37 +1,102 @@
 // 312. Burst Balloons
 // https://leetcode.com/problems/burst-balloons/
 
-/* we can find that for any balloons left the maxCoins does not depends on the balloons already bursted. 
-  This indicate that we can use memorization (top down) or dynamic programming (bottom up) for all the cases 
-  from small numbers of balloon until n balloons. 
-  
-  中心开花吗？
+// https://leetcode.com/discuss/72216/share-some-analysis-and-explanations
 
-  O(n ^ 3)
-*/
+// Dynamic Programmming - O(n ^ 3)
 
 public class Solution {
     
     public int maxCoins(int[] nums) {
-        int n = nums.length + 2;
-        
-        int[] arr = new int[n];
-        for (int i = 1; i < n - 1; i++) {
-            arr[i] = nums[i - 1];
+        int[] extNums = new int[nums.length + 2];
+        for (int i = 0; i < nums.length; i++) {
+            extNums[i + 1] = nums[i];
         }
-        arr[0] = arr[n - 1] = 1;
+        extNums[0] = extNums[extNums.length - 1] = 1;
+
+        int[][] dp = new int[extNums.length][extNums.length];
         
-        /* M[i][j] represents the cost of bursting balloons between i and j exclusively */ 
-        int[][] M = new int[n][n];
-        for (int k = 2; k < n; k++) {
-            for (int left = 0; left < n - k; left++) {
-                int right = left + k;
-                for (int i = left + 1; i < right; i++) {
-                    M[left][right] = Math.max(M[left][right], arr[left] * arr[i] * arr[right] + M[left][i] + M[i][right]);
+        for (int i = extNums.length - 1; i >= 0; i--) {
+            for (int j = i + 1; j < extNums.length; j++) {
+                for (int t = i + 1; t < j; t++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][t] + dp[t][j] + extNums[i] * extNums[t] * extNums[j]);
                 }
             }
         }
         
-        return M[0][n - 1];
+        return dp[0][extNums.length - 1];
+    }
+}
+
+// Divide and Conquer with Memorization
+
+public class Solution {
+
+    public int maxCoins(int[] nums) {
+        int[] extNums = new int[nums.length + 2];
+        for (int i = 0; i < nums.length; i++) {
+            extNums[i + 1] = nums[i];
+        }
+        extNums[0] = extNums[extNums.length - 1] = 1;
+
+        int[][] memo = new int[extNums.length][extNums.length];
+        
+        return dfs(memo, extNums, 0, extNums.length - 1);
+    }
+    
+    private int dfs(int[][] memo, int[] extNums, int left, int right) {
+        if (left + 1 == right) {
+            return 0;
+        }
+        if (memo[left][right] != 0) {
+            return memo[left][right];
+        }
+        
+        int res = 0;
+        
+        for (int i = left + 1; i < right; i++) {
+            res = Math.max(res, dfs(memo, extNums, left, i) + extNums[left] * extNums[i] * extNums[right] + dfs(memo, extNums, i, right));
+        }
+        
+        memo[left][right] = res;
+        return res;
+    }
+}
+
+// DFS - O(n!)
+
+public class Solution {
+
+    public int maxCoins(int[] nums) {
+        int[] res = new int[1];
+        int[] curRes = new int[1];
+        
+        List<Integer> curList = new ArrayList<>();
+        curList.add(1);
+        for (int num : nums) {
+            curList.add(num);
+        }
+        curList.add(1);
+        
+        dfs(res, curRes, curList);
+        return res[0];
+    }
+    
+    private void dfs(int[] res, int[] curRes, List<Integer> curList) {
+        if (curList.size() == 2) {
+            res[0] = Math.max(res[0], curRes[0]);
+            return;
+        }
+        
+        // select one of the balloon to burst
+        for (int i = 1; i < curList.size() - 1; i++) {
+            int coin = curList.get(i - 1) * curList.get(i) * curList.get(i + 1);
+            int balloon = curList.get(i);
+            curRes[0] += coin;
+            curList.remove(i);
+            dfs(res, curRes, curList);
+            curRes[0] -= coin;
+            curList.add(i, balloon);
+        }
     }
 }
