2a29e44755efbe466790aa0ffdbeb5ed0cc9bc88
Hefang Li <hefangli0412@users.noreply.github.com>
2016-05-18 15:46:25 -0700

    Update and rename 284.java to iterator/284.java

--- iterator/284.java
+++ iterator/284.java
@@ -44,3 +44,47 @@ class PeekingIterator implements Iterator<Integer> {
 	    return hasPeeked || iterator.hasNext();
 	}
 }
+
+
+// 一个variable不行吗？
+
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+class PeekingIterator implements Iterator<Integer> {
+    
+    Iterator<Integer> iterator;
+    Integer peekValue;
+    
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
+	    this.iterator = iterator;
+	    peekValue = null;
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        if (peekValue == null) {
+            peekValue = iterator.next();
+        }
+        
+        return peekValue;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    if (peekValue != null) {
+	        Integer result = peekValue;
+	        peekValue = null;
+	        return result;
+	    }
+	    
+	    return iterator.next();
+	}
+
+	@Override
+	public boolean hasNext() {
+	    return peekValue != null || iterator.hasNext();
+	}
+}
