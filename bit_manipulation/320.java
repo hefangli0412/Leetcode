4edd979c7609f5163a19d413f3397c011abb59e3
Hefang Li <hefangli0412@users.noreply.github.com>
2016-05-19 17:50:22 -0700

    Update 320.java

--- bit_manipulation/320.java
+++ bit_manipulation/320.java
@@ -1,29 +1,30 @@
 // 320. Generalized Abbreviation
 
 public class Solution {
-    public List<String> generateAbbreviations(String word) {
-        List<String> result = new ArrayList<>();
-        
+	
+	public List<String> generateAbbreviations(String word) {
+		List<String> result = new ArrayList<>();
+
 		int n = word.length();
 		// get 2's power
 		int max = (1 << n) - 1;
 
 		for (int i = 0; i <= max; i++) {
-            StringBuilder sb = new StringBuilder();
-            int zeroCount = 0;
+			StringBuilder sb = new StringBuilder();
+			int zeroCount = 0;
 			for (int j = 0; j < n; j++) {
 				if ((i & (1 << j)) != 0) {
 					if (zeroCount > 0) {
-					    sb.append(zeroCount);
-					    zeroCount = 0;
+						sb.append(zeroCount);
+						zeroCount = 0;
 					}
 					sb.append(word.charAt(j));
 				} else {
-				    zeroCount++;
+					zeroCount++;
 				}
 			}
 			if (zeroCount > 0) {
-			    sb.append(zeroCount);
+				sb.append(zeroCount);
 			}
 			result.add(sb.toString());
 		}
