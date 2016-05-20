a05379ed94026783b8ffdad9412418a2de0876c1
Hefang Li <hefangli0412@users.noreply.github.com>
2016-05-19 17:24:52 -0700

    Update 78.java

--- bit_manipulation/78.java
+++ bit_manipulation/78.java
@@ -1,27 +1,29 @@
 // https://leetcode.com/problems/subsets/
 
 public class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return result;
-        }
-        
-        Arrays.sort(nums);
-        
-        int n = nums.length;
-        // get 2's power
-	int max = (1 << n) - 1;
-	
-	for (int i = 0; i <= max; i++) {
-		List<Integer> subset = new ArrayList<>();
-		for (int j = 0; j < n; j++) {
-			if ((i & (1 << j)) != 0) {
-			    subset.add(nums[j]);
-			}
+
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (nums == null || nums.length == 0) {
+			return result;
 		}
-		result.add(subset);
+
+		Arrays.sort(nums);
+
+		int n = nums.length;
+		// get 2's power
+		int max = (1 << n) - 1;
+
+		for (int i = 0; i <= max; i++) {
+			List<Integer> subset = new ArrayList<>();
+			for (int j = 0; j < n; j++) {
+				if ((i & (1 << j)) != 0) {
+					subset.add(nums[j]);
+				}
+			}
+			result.add(subset);
+		}
+		return result;
 	}
-	return result;
-    }
+	
 }
